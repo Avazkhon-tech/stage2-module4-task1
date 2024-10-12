9ec9c3e4f44ba6cdc1470806b6e3cc48472d451d
Avazkhon <avazkhonnazirov33@gmail.com>
2024-10-12 23:44:11 +0500

    first commit

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,25 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
+import com.mjc.stage2.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+
+    @Override
+    public Connection createConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("org.h2.Driver");
+            connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
+        } catch (ClassNotFoundException e) {
+            System.err.println("H2 Driver not found: " + e.getMessage());
+        } catch (SQLException e) {
+            System.err.println("Failed to create connection: " + e.getMessage());
+        }
+        return connection;
+    }
 }
+
 
