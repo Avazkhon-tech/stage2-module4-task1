68c8fe4ac67d9e7c20ec149e678844df1e81351b
Avazkhon <avazkhonnazirov33@gmail.com>
2024-10-13 00:08:04 +0500

    second commit

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,13 +1,16 @@
 package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
-    private static ThreadSafeSingleton instance = new ThreadSafeSingleton();
+    private static ThreadSafeSingleton instance;
 
 
     private ThreadSafeSingleton() {
     }
 
-    public static ThreadSafeSingleton getInstance() {
+    public static synchronized ThreadSafeSingleton getInstance() {
+        if (instance == null) {
+            instance = new ThreadSafeSingleton();
+        }
         return instance;
     }
 }
